d8dc67e8d97490a4925d7ff5dc7fa9d6383064e0
Miguel Magalhaes <magamig@gmail.com>
2019-11-27 16:32:17 +0000

    login and register forms

---
+++ components/LoginForm/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import {
+	Button,
+	TextField
+} from '@material-ui/core';
+
+const LoginForm = props => (
+	<div>
+		<TextField
+			required
+			fullWidth
+			autoFocus
+			margin="normal"
+			id="email"
+			key="email"
+			autoComplete="email"
+			label="Email"
+		/>
+		<TextField
+			required
+			fullWidth
+			margin="normal"
+			id="password"
+			key="password"
+			type="password"
+			autoComplete="current-password"
+			label="Password"
+		/>
+		<Button
+			fullWidth
+			variant="contained">
+			Login
+		</Button>
+	</div>
+);
+
+export default LoginForm;
---
+++ pages/login.js
@@ -0,0 +1,13 @@
+import LoginForm from '../components/LoginForm';
+import Typography from '@material-ui/core/Typography';
+
+const Login = () => (
+	<div>
+		<Typography variant="h1">
+			login
+		</Typography>
+		<LoginForm />
+	</div>
+);
+
+export default Login;
