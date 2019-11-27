0f3d1c079e40a0a37204c6e1ec238b793aa1c88e
Miguel Magalhaes <magamig@gmail.com>
2019-11-27 16:32:17 +0000

    login and register forms

---
+++ pages/register.js
@@ -0,0 +1,13 @@
+import RegisterForm from '../components/RegisterForm';
+import Typography from '@material-ui/core/Typography';
+
+const Login = () => (
+	<div>
+		<Typography variant="h1">
+			register
+		</Typography>
+		<RegisterForm />
+	</div>
+);
+
+export default Login;
