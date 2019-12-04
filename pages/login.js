c9c0d88b701bd5ad188a771b5abc0f6dce44c98f
Miguel Magalhaes <magamig@gmail.com>
2019-12-04 12:41:06 +0000

    map

---
+++ pages/login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import LoginForm from '../components/LoginForm';
+import Typography from '@material-ui/core/Typography';
+
+const Login = () => (
+	<div>
+		<Typography variant="h1">
+			login
+		</Typography>
+		<LoginForm />
+	</div>
+);
+
+export default Login;
