de6178014a0d0b36b439b372fe39097bb9e31355
apedrob <antoniopedromb@gmail.com>
2019-12-06 01:49:34 +0000

    Fix merge conflits

--- components/LoginForm/index.js
+++ components/LoginForm/index.js
@@ -1,55 +1,32 @@
-import React from 'react';
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-import {Button, TextField} from '@material-ui/core';
+import React from "react";
+import { Button, TextField } from "@material-ui/core";
 
 const LoginForm = () => (
-=======
-import {
-	Button,
-	TextField
-} from '@material-ui/core';
-
-const LoginForm = props => (
->>>>>>> d8dc67e... login and register forms
-=======
-import {Button, TextField} from '@material-ui/core';
-
-const LoginForm = () => (
->>>>>>> 1ae5e65... eslint added
-=======
-import {Button, TextField} from '@material-ui/core';
-
-const LoginForm = () => (
->>>>>>> 117c71f04e34aef1fc52ba817b10806abe9a0919
-	<div>
-		<TextField
-			required
-			fullWidth
-			autoFocus
-			margin="normal"
-			id="email"
-			key="email"
-			autoComplete="email"
-			label="Email"
-		/>
-		<TextField
-			required
-			fullWidth
-			margin="normal"
-			id="password"
-			key="password"
-			type="password"
-			autoComplete="current-password"
-			label="Password"
-		/>
-		<Button
-			fullWidth
-			variant="contained">
-			Login
-		</Button>
-	</div>
+  <div>
+    <TextField
+      required
+      fullWidth
+      autoFocus
+      margin="normal"
+      id="email"
+      key="email"
+      autoComplete="email"
+      label="Email"
+    />
+    <TextField
+      required
+      fullWidth
+      margin="normal"
+      id="password"
+      key="password"
+      type="password"
+      autoComplete="current-password"
+      label="Password"
+    />
+    <Button fullWidth variant="contained">
+      Login
+    </Button>
+  </div>
 );
 
 export default LoginForm;
--- components/RegisterForm/index.js
+++ components/RegisterForm/index.js
@@ -1,93 +1,64 @@
-import React from 'react';
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-import {Button, FormControl, FormControlLabel, FormLabel, Radio, RadioGroup, TextField} from '@material-ui/core';
-
-const RegisterForm = () => (
-=======
+import React from "react";
 import {
-	Button,
-	TextField,
-	FormControl,
-	FormLabel,
-	RadioGroup,
-	RadioButton,
-	Radio,
-	FormHelperText,
-	FormControlLabel
-} from '@material-ui/core';
-
-const RegisterForm = props => (
->>>>>>> d8dc67e... login and register forms
-=======
-import {Button, FormControl, FormControlLabel, FormLabel, Radio, RadioGroup, TextField} from '@material-ui/core';
+  Button,
+  FormControl,
+  FormControlLabel,
+  FormLabel,
+  Radio,
+  RadioGroup,
+  TextField
+} from "@material-ui/core";
 
 const RegisterForm = () => (
->>>>>>> 1ae5e65... eslint added
-=======
-import {Button, FormControl, FormControlLabel, FormLabel, Radio, RadioGroup, TextField} from '@material-ui/core';
-
-const RegisterForm = () => (
->>>>>>> 117c71f04e34aef1fc52ba817b10806abe9a0919
-	<div>
-		<TextField
-			required
-			fullWidth
-			autoFocus
-			margin="normal"
-			id="name"
-			key="name"
-			autoComplete="name"
-			label="Name"
-		/>
-		<TextField
-			required
-			fullWidth
-			margin="normal"
-			id="email"
-			key="email"
-			autoComplete="email"
-			label="Email"
-		/>
-		<TextField
-			required
-			fullWidth
-			margin="normal"
-			id="password"
-			key="password"
-			type="password"
-			autoComplete="current-password"
-			label="Password"
-		/>
-		<FormControl component="fieldset">
-			<FormLabel component="legend">Role</FormLabel>
-			<RadioGroup defaultValue="beelover" name="role">
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-				<FormControlLabel value="beelover" control={<Radio/>} label="BeeLover"/>
-				<FormControlLabel value="beekeeper" control={<Radio/>} label="BeeKeeper"/>
-=======
-				<FormControlLabel value="beelover" control={<Radio />} label="BeeLover" />
-				<FormControlLabel value="beekeeper" control={<Radio />} label="BeeKeeper" />
->>>>>>> d8dc67e... login and register forms
-=======
-				<FormControlLabel value="beelover" control={<Radio/>} label="BeeLover"/>
-				<FormControlLabel value="beekeeper" control={<Radio/>} label="BeeKeeper"/>
->>>>>>> 1ae5e65... eslint added
-=======
-				<FormControlLabel value="beelover" control={<Radio/>} label="BeeLover"/>
-				<FormControlLabel value="beekeeper" control={<Radio/>} label="BeeKeeper"/>
->>>>>>> 117c71f04e34aef1fc52ba817b10806abe9a0919
-			</RadioGroup>
-		</FormControl>
-		<Button
-			fullWidth
-			variant="contained">
-			Register
-		</Button>
-	</div>
+  <div>
+    <TextField
+      required
+      fullWidth
+      autoFocus
+      margin="normal"
+      id="name"
+      key="name"
+      autoComplete="name"
+      label="Name"
+    />
+    <TextField
+      required
+      fullWidth
+      margin="normal"
+      id="email"
+      key="email"
+      autoComplete="email"
+      label="Email"
+    />
+    <TextField
+      required
+      fullWidth
+      margin="normal"
+      id="password"
+      key="password"
+      type="password"
+      autoComplete="current-password"
+      label="Password"
+    />
+    <FormControl component="fieldset">
+      <FormLabel component="legend">Role</FormLabel>
+      <RadioGroup defaultValue="beelover" name="role">
+        <FormControlLabel
+          value="beelover"
+          control={<Radio />}
+          label="BeeLover"
+        />
+        <FormControlLabel
+          value="beekeeper"
+          control={<Radio />}
+          label="BeeKeeper"
+        />
+      </RadioGroup>
+    </FormControl>
+    <Button fullWidth variant="contained">
+      Register
+    </Button>
+  </div>
 );
 
 export default RegisterForm;
