9636a62ad871b0aaf9b9bbc346e577f40559cb72
David Kramer <davidwarrock24a@gmail.com>
2020-01-03 15:11:09 +0000

    Revert "Linechart with design (#17)" (#24)
    This reverts commit 0794c67a48f7804a1d358bfa10fca2d70f11b4d9.

--- components/LineChart/index.js
+++ components/LineChart/index.js
@@ -1,120 +1,131 @@
-/// <reference path="../Plot/react-vis.d.ts"/>
-
-import React, { Component } from "react";
-import Button from "@material-ui/core/Button";
-import "react-vis/dist/style.css";
+import React, { Component } from 'react';
+import {curveCatmullRom} from 'd3-shape';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import Button from '@material-ui/core/Button';
+import 'react-vis/dist/style.css';
+import moment from "moment";
 import {
   XYPlot,
   XAxis,
   YAxis,
   HorizontalGridLines,
   VerticalGridLines,
-  LineSeries,
-  FlexibleXYPlot,
-  FlexibleWidthXYPlot,
-  FlexibleHeightXYPlot
-} from "react-vis";
-import { Container } from "@material-ui/core";
-import { withStyles } from "@material-ui/core/styles";
+  LineSeries
+} from 'react-vis';
+import my_data from '../Plot/test.json';
+import my_data2 from '../Plot/test2.json';
 
-const styles = theme => ({
-  ButtonContent: {
-    padding: theme.spacing(1, 5, 6),
-    marginTop: 6
-  }
-});
+const TYPE = [my_data, my_data2, my_data, my_data2, my_data];
+
+const processLabel = (data) => {
+  const newdata = data.map (row => (
+    moment(row.date).format("H:mm").toString()
+
+    //"A"
+  ));
+  /* this.setState({loading: false}) */
+  return newdata
+};
+
+const processData = (data) => {
+    return data.map((row, i) => ({
+      x: i,
+      y: row.temperature  //dps vai ter q ser value
+    }));
+  };
 
 class LineChart extends Component {
-  render() {
-    const { labels, values } = this.props;
-    const { classes } = this.props;
-    const error =
-      this.props.labels.length == 0 && this.props.values.length == 0;
+  constructor(){
+      super();
+      this.state = {
+        loading : false,
+        data: {
+          values: [], 
+          labels: []
+        },
+      };
+  }
+
+  handleChangeData = type => {    
+    const PLOT_DATA = TYPE[type || 0].data;
+    const data = { values: processData(PLOT_DATA), labels: processLabel(PLOT_DATA) };
+
+    this.setState({ data });
+  }
+
+  componentDidMount(){
+    this.handleChangeData(null);
+  }
+  
+
+  
+  render (){
+    const { loading, data } = this.state;
+    const { labels, values } = data;
 
     return (
-      <Container>
-        <FlexibleXYPlot width={500} height={400} animation={true}>
-          <HorizontalGridLines style={{ stroke: "#B7E9ED" }} />
-          <VerticalGridLines style={{ stroke: "#B7E9ED" }} />
-          <XAxis
+      <div>
+      {!loading ?  
+        <XYPlot width={1000} height={500} animation={true}>
+        <HorizontalGridLines style={{stroke: '#B7E9ED'}} />
+        <VerticalGridLines style={{stroke: '#B7E9ED'}} />
+        <XAxis
             title="X Axis"
             tickFormat={v => labels[v]}
             tickLabelAngle={-45}
             style={{
-              line: { stroke: "#ADDDE1" },
-              ticks: { stroke: "#ADDDE1" },
-              text: { stroke: "none", fill: "#6b6b76", fontWeight: 600 }
+            line: {stroke: '#ADDDE1'},
+            ticks: {stroke: '#ADDDE1'},
+            text: {stroke: 'none', fill: '#6b6b76', fontWeight: 600}
             }}
-          />
 
-          <YAxis title="Y Axis" />
-          <LineSeries
+        />
+        <YAxis title="Y Axis" />
+        <LineSeries
             className="first-series"
             data={values}
             style={{
-              strokeLinejoin: "round",
-              strokeWidth: 4
+            strokeLinejoin: 'round',
+            strokeWidth: 4
             }}
-          />
-        </FlexibleXYPlot>
+        />
+        <LineSeries className="second-series" data={null} />
+        <LineSeries
+            className="third-series"
+            curve={'curveMonotoneX'}
+            data={[{x: 17, y: 100}, {x: 2, y: 4}, {x: 3, y: 2}, {x: 4, y: 15}]}
+            strokeDasharray="7, 3"
+        />
+        <LineSeries
+            className="fourth-series"
+            curve={curveCatmullRom.alpha(0.5)}
+            data={[{x: 1, y: 7}, {x: 2, y: 11}, {x: 3, y: 9}, {x: 4, y: 2}]}
+        />
+        </XYPlot>
+        :''}
 
-        {!error ? (
-          <Container className={classes.ButtonContent}>
-            <Button
-              variant="outlined"
-              color="secondary"
-              size="small"
-              onClick={() => this.props.handleChangeData("temperature")}
-            >
-              Temperature
+
+        <ButtonGroup aria-label="Basic example">
+            <Button variant="outlined" color="primary" onClick={() => this.handleChangeData(0)}>
+            Temperature
             </Button>
-            <Button
-              variant="outlined"
-              color="secondary"
-              size="small"
-              onClick={() => this.props.handleChangeData("pressure")}
-            >
-              Pressure
+            <Button variant="outlined" color="primary" onClick={() => this.handleChangeData(1)}>
+            Pressure
             </Button>
-            <Button
-              variant="outlined"
-              color="secondary"
-              size="small"
-              onClick={() => this.props.handleChangeData("light")}
-            >
-              Light
+            <Button variant="outlined" color="primary" onClick={() => this.handleChangeData(2)}>
+            Light
             </Button>
-            <Button
-              variant="outlined"
-              color="secondary"
-              size="small"
-              onClick={() => this.props.handleChangeData("noise")}
-            >
-              Noise
+            <Button variant="outlined" color="primary" onClick={() => this.handleChangeData(3)}>
+            Noise
             </Button>
-            <Button
-              variant="outlined"
-              color="secondary"
-              size="small"
-              onClick={() => this.props.handleChangeData("accelerometer")}
-            >
-              Accelerometer
+            <Button variant="outlined" color="primary" onClick={() => this.handleChangeData(4)}>
+            Accelerometer
             </Button>
-            <Button
-              variant="outlined"
-              color="secondary"
-              size="small"
-              onClick={() => this.props.handleChangeData("humidity")}
-            >
-              Humidity
-            </Button>
-          </Container>
-        ) : (
-          ""
-        )}
-      </Container>
+        </ButtonGroup>
+        
+        </div>    
     );
-  }
+    }
 }
 
-export default withStyles(styles)(LineChart);
+export default LineChart;
