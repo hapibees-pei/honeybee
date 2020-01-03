4d4f4d28313abc7e4eaf493c8ace4053dcfecbb6
David Kramer <davidwarrock24a@gmail.com>
2020-01-03 16:54:30 +0000

    Add statistics page (#26)
    * Linechart with design (#17)
    
    * Linechart with design
    
    * Finish statistics page without logic
    
    * Integrate both charts with backend
    
    * Add LineChart logic in higher component
    
    * Fix react vis

--- components/LineChart/index.js
+++ components/LineChart/index.js
@@ -1,131 +1,117 @@
-import React, { Component } from 'react';
-import {curveCatmullRom} from 'd3-shape';
-import ButtonGroup from '@material-ui/core/ButtonGroup';
-import Button from '@material-ui/core/Button';
-import 'react-vis/dist/style.css';
-import moment from "moment";
+// <reference path="../Plot/react-vis.d.ts"/>
+import React, { Component } from "react";
+import Button from "@material-ui/core/Button";
+//import "react-vis/dist/style.css";
 import {
   XYPlot,
   XAxis,
   YAxis,
   HorizontalGridLines,
   VerticalGridLines,
-  LineSeries
-} from 'react-vis';
-import my_data from '../Plot/test.json';
-import my_data2 from '../Plot/test2.json';
+  LineSeries,
+  FlexibleXYPlot
+} from "react-vis";
+import { Container } from "@material-ui/core";
+import { withStyles } from "@material-ui/core/styles";
 
-const TYPE = [my_data, my_data2, my_data, my_data2, my_data];
-
-const processLabel = (data) => {
-  const newdata = data.map (row => (
-    moment(row.date).format("H:mm").toString()
-
-    //"A"
-  ));
-  /* this.setState({loading: false}) */
-  return newdata
-};
-
-const processData = (data) => {
-    return data.map((row, i) => ({
-      x: i,
-      y: row.temperature  //dps vai ter q ser value
-    }));
-  };
+const styles = theme => ({
+  ButtonContent: {
+    padding: theme.spacing(1, 5, 6),
+    marginTop: 6
+  }
+});
 
 class LineChart extends Component {
-  constructor(){
-      super();
-      this.state = {
-        loading : false,
-        data: {
-          values: [], 
-          labels: []
-        },
-      };
-  }
-
-  handleChangeData = type => {    
-    const PLOT_DATA = TYPE[type || 0].data;
-    const data = { values: processData(PLOT_DATA), labels: processLabel(PLOT_DATA) };
-
-    this.setState({ data });
-  }
-
-  componentDidMount(){
-    this.handleChangeData(null);
-  }
-  
-
-  
-  render (){
-    const { loading, data } = this.state;
-    const { labels, values } = data;
+  render() {
+    const { labels, values } = this.props;
+    const { classes } = this.props;
+    const error =
+      this.props.labels.length == 0 && this.props.values.length == 0;
 
     return (
-      <div>
-      {!loading ?  
-        <XYPlot width={1000} height={500} animation={true}>
-        <HorizontalGridLines style={{stroke: '#B7E9ED'}} />
-        <VerticalGridLines style={{stroke: '#B7E9ED'}} />
-        <XAxis
+      <Container>
+        <FlexibleXYPlot width={500} height={400} animation={true}>
+          <HorizontalGridLines style={{ stroke: "#B7E9ED" }} />
+          <VerticalGridLines style={{ stroke: "#B7E9ED" }} />
+          <XAxis
             title="X Axis"
             tickFormat={v => labels[v]}
             tickLabelAngle={-45}
             style={{
-            line: {stroke: '#ADDDE1'},
-            ticks: {stroke: '#ADDDE1'},
-            text: {stroke: 'none', fill: '#6b6b76', fontWeight: 600}
+              line: { stroke: "#ADDDE1" },
+              ticks: { stroke: "#ADDDE1" },
+              text: { stroke: "none", fill: "#6b6b76", fontWeight: 600 }
             }}
+          />
 
-        />
-        <YAxis title="Y Axis" />
-        <LineSeries
+          <YAxis title="Y Axis" />
+          <LineSeries
             className="first-series"
             data={values}
             style={{
-            strokeLinejoin: 'round',
-            strokeWidth: 4
+              strokeLinejoin: "round",
+              strokeWidth: 4
             }}
-        />
-        <LineSeries className="second-series" data={null} />
-        <LineSeries
-            className="third-series"
-            curve={'curveMonotoneX'}
-            data={[{x: 17, y: 100}, {x: 2, y: 4}, {x: 3, y: 2}, {x: 4, y: 15}]}
-            strokeDasharray="7, 3"
-        />
-        <LineSeries
-            className="fourth-series"
-            curve={curveCatmullRom.alpha(0.5)}
-            data={[{x: 1, y: 7}, {x: 2, y: 11}, {x: 3, y: 9}, {x: 4, y: 2}]}
-        />
-        </XYPlot>
-        :''}
+          />
+        </FlexibleXYPlot>
 
-
-        <ButtonGroup aria-label="Basic example">
-            <Button variant="outlined" color="primary" onClick={() => this.handleChangeData(0)}>
-            Temperature
+        {!error ? (
+          <Container className={classes.ButtonContent}>
+            <Button
+              variant="outlined"
+              color="secondary"
+              size="small"
+              onClick={() => this.props.handleChangeData("temperature")}
+            >
+              Temperature
             </Button>
-            <Button variant="outlined" color="primary" onClick={() => this.handleChangeData(1)}>
-            Pressure
+            <Button
+              variant="outlined"
+              color="secondary"
+              size="small"
+              onClick={() => this.props.handleChangeData("pressure")}
+            >
+              Pressure
             </Button>
-            <Button variant="outlined" color="primary" onClick={() => this.handleChangeData(2)}>
-            Light
+            <Button
+              variant="outlined"
+              color="secondary"
+              size="small"
+              onClick={() => this.props.handleChangeData("light")}
+            >
+              Light
             </Button>
-            <Button variant="outlined" color="primary" onClick={() => this.handleChangeData(3)}>
-            Noise
+            <Button
+              variant="outlined"
+              color="secondary"
+              size="small"
+              onClick={() => this.props.handleChangeData("noise")}
+            >
+              Noise
             </Button>
-            <Button variant="outlined" color="primary" onClick={() => this.handleChangeData(4)}>
-            Accelerometer
+            <Button
+              variant="outlined"
+              color="secondary"
+              size="small"
+              onClick={() => this.props.handleChangeData("accelerometer")}
+            >
+              Accelerometer
             </Button>
-        </ButtonGroup>
-        
-        </div>    
+            <Button
+              variant="outlined"
+              color="secondary"
+              size="small"
+              onClick={() => this.props.handleChangeData("humidity")}
+            >
+              Humidity
+            </Button>
+          </Container>
+        ) : (
+          ""
+        )}
+      </Container>
     );
-    }
+  }
 }
 
-export default LineChart;
+export default withStyles(styles)(LineChart);
---
+++ pages/statistics.js
@@ -0,0 +1,196 @@
+import React, { Component } from "react";
+import LoginForm from "../components/LoginForm";
+import { CssBaseline, Typography, Container } from "@material-ui/core";
+import { withStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import LineChart from "../components/LineChart/index.js";
+import OverallChart from "../components/OverallChart/index.js";
+import axios from "axios";
+import { Footer, Header } from "../components";
+import moment from "moment";
+
+const styles = theme => ({
+  "@global": {
+    body: {
+      backgroundColor: theme.palette.common.white
+    },
+    ul: {
+      margin: 0,
+      padding: 0
+    },
+    li: {
+      listStyle: "none"
+    }
+  },
+  root: {
+    flexGrow: 1
+  },
+  statisticsContent: {
+    padding: theme.spacing(8, 0, 6),
+    marginTop: 0,
+    marginBottom: 50
+  },
+  plot: {
+    padding: theme.spacing(4, 0, 6)
+  }
+});
+
+const APIARY_API = "http://localhost:3001/api/v1/apiaries/";
+const HIVE_PARAMS = [
+  "temperature",
+  "pressure",
+  "light",
+  "noise",
+  "accelerometer",
+  "humidity"
+];
+
+class StatisticsPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      apiary_id: "",
+      hive_id: "",
+      overallValues: [],
+      linechartValues: [],
+      linechartLabels: [],
+      error: ""
+    };
+  }
+
+  processLabel = stats => {
+    const newdata = stats.data.map(row =>
+      moment(row.date)
+        .format("H:mm")
+        .toString()
+    );
+
+    return newdata;
+  };
+
+  processData = stats => {
+    return stats.data.map((row, i) => ({
+      x: i,
+      y: row.value
+    }));
+  };
+
+  lastReading = readings => {
+    return readings.data[readings.data.length - 1].value.toFixed(2);
+  };
+
+  fetchLineChartData = type => {
+    axios
+      .get(
+        APIARY_API +
+          this.state.apiary_id +
+          "/statistics/" +
+          this.state.hive_id +
+          "?query=" +
+          type +
+          "&time_unity=minute"
+      )
+      .then(res => {
+        this.setState({
+          linechartValues: this.processData(res.data),
+          linechartLabels: this.processLabel(res.data)
+        });
+      })
+      .catch(error => {
+        this.setState({
+          error: "Error Fetch LineChart Data",
+          linechartLabels: [],
+          linechartValues: []
+        });
+      });
+  };
+
+  fetchOverallData = async () => {
+    var readings = [];
+
+    for (var i = 0; i < HIVE_PARAMS.length; i++) {
+      await axios
+        .get(
+          APIARY_API +
+            this.state.apiary_id +
+            "/statistics/" +
+            this.state.hive_id +
+            "?query=" +
+            HIVE_PARAMS[i] +
+            "&time_unity=minute"
+        )
+        .then(res => {
+          readings.push(this.lastReading(res.data));
+        })
+        .catch(error => {
+          readings = new Array(6).fill("error");
+          this.setState({ error: "Error Fetch Overall Data" });
+        });
+    }
+
+    this.setState({ overallValues: readings });
+  };
+
+  componentDidMount() {
+    var params = {};
+    location.search
+      .slice(1)
+      .split("&")
+      .map(a => {
+        params[a.split("=")[0]] = a.split("=")[1];
+      });
+
+    this.setState(
+      {
+        apiary_id: params.apiary,
+        hive_id: params.hive
+      },
+      () => {
+        this.fetchOverallData();
+        this.fetchLineChartData("temperature");
+      }
+    );
+  }
+
+  render() {
+    const { classes } = this.props;
+    const { overallValues, linechartValues, linechartLabels } = this.state;
+
+    return (
+      <React.Fragment>
+        <CssBaseline />
+        <Header />
+        <Container maxWidth="90%" className={classes.statisticsContent}>
+          <Typography component="h1" variant="h2" align="center">
+            Hive X
+          </Typography>
+          <div className={classes.root}>
+            <Grid
+              container
+              justify="center"
+              align="center"
+              item xs
+              direction="row"
+              spacing={3}
+              className={classes.plot}
+            >
+              <Grid item xs sm>
+                {<OverallChart values={overallValues} />}
+              </Grid>
+              <Grid item xs sm>
+                <LineChart
+                  values={linechartValues}
+                  labels={linechartLabels}
+                  handleChangeData={this.fetchLineChartData}
+                />
+              </Grid>
+            </Grid>
+          </div>
+        </Container>
+        <Footer />
+      </React.Fragment>
+    );
+  }
+}
+
+export default withStyles(styles)(StatisticsPage);
