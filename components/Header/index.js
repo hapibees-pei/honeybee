557ec9a27024c68ea363a3fcef64a0b296a7d5cf
apedrob <a78305@alunos.uminho.pt>
2020-01-12 22:14:50 +0000

    change branch head

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,12 +9,18 @@ import {
   Container
 } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-import Logo from "../../assets/images/logo/black_on_transparent.png";
+import { withStyles } from "@material-ui/core/styles";
+import Logo from "../../public/black_on_transparent.png";
 
-const useStyles = makeStyles(theme => ({
+const styles = theme => ({
   appBar: {
     borderBottom: `1px solid ${theme.palette.divider}`,
-    backgroundColor: `rgb(252, 195, 34)`
+    backgroundColor: theme.palette.primary.main
+  },
+  appBar2: {
+    borderBottom: `1px solid ${theme.palette.divider}`,
+    backgroundColor: 'white',
+    opacity: 0.8
   },
   toolbar: {
     flexDirection: "row",
@@ -32,91 +38,116 @@ const useStyles = makeStyles(theme => ({
     margin: theme.spacing(2, 1),
     alignSelf: "center"
   }
-}));
+});
 
-function Header(props) {
-  const classes = useStyles();
-  const isLogin = typeof window !== "undefined" && localStorage.user;
-  const handleLogout = () => {
-    localStorage.clear();
-    setLogin(false);
-  };
-  const [login, setLogin] = useState(isLogin);
-  return (
-    <AppBar
-      position="sticky"
-      color="default"
-      elevation={0}
-      className={classes.appBar}
-    >
-      <Container maxWidth="md">
-        {login ? (
-          <Toolbar className={classes.toolbar}>
-            <Link className={classes.toolbarLogo} href="/">
-              <img src={Logo} height="40px" />
-            </Link>
-            <div>
-              {" "}
-              <Button
-                variant="contained"
-                color="secondary"
-                className={classes.link}
-                href="/beekeeper/hives"
-              >
-                Hives
-              </Button>
-              <Button
-                variant="contained"
-                color="secondary"
-                className={classes.link}
-                href="/beekeeper/apiary"
-              >
-                Add Apiary
-              </Button>
-            </div>
-            <Link
-              variant="button"
-              color="secondary"
-              href="/register"
-              className={classes.link}
-            >
-              {"Hello " + localStorage.user}
-            </Link>
-            <Button
-              onClick={handleLogout}
-              color="secondary"
-              variant="outlined"
-              className={classes.link}
-            >
-              Logout
-            </Button>
-          </Toolbar>
-        ) : (
-          <Toolbar className={classes.toolbar}>
-            <Link className={classes.toolbarLogo} href="/">
-              <img src={Logo} height="40px" />
-            </Link>
-            <Link
-              variant="button"
-              color="secondary"
-              href="/register"
-              className={classes.link}
-            >
-              Register
-            </Link>
-            <Button
-              href="/login"
-              color="secondary"
-              variant="outlined"
-              className={classes.link}
-            >
-              Login
-            </Button>
-          </Toolbar>
-        )}
-      </Container>
-    </AppBar>
-  );
+class Header extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLogin: typeof window !== "undefined" && localStorage.user,
+      login: null,
+      setLogin: null,
+      isTop:  true
+    };
+  }
+
+  componentDidMount() {
+    document.addEventListener('scroll', () => {
+      const isTop = window.scrollY < 500;
+      if (isTop !== this.state.isTop) {
+        this.setState({ isTop })
+      }
+    });
+  }
+
+  render() {
+
+    const handleLogout = () => {
+      localStorage.clear();
+      const [login, setLogin] = useState(this.state.isLogin);
+      setLogin(false);
+    };
+
+    const { login } = this.state;
+    const { classes } = this.props;
+
+    return (
+        <AppBar
+            position="sticky"
+            color="default"
+            elevation={0}
+            className={this.state.isTop ? classes.appBar : classes.appBar2}
+        >
+          <Container maxWidth="md">
+            {login ? (
+                <Toolbar className={classes.toolbar}>
+                  <Link className={classes.toolbarLogo} href="/">
+                    <img src={Logo} height="40px" />
+                  </Link>
+                  <div>
+                    {" "}
+                    <Button
+                        variant="contained"
+                        color="secondary"
+                        className={classes.link}
+                        href="/beekeeper/hives"
+                    >
+                      Hives
+                    </Button>
+                    <Button
+                        variant="contained"
+                        color="secondary"
+                        className={classes.link}
+                        href="/beekeeper/apiary"
+                    >
+                      Add Apiary
+                    </Button>
+                  </div>
+                  <Link
+                      variant="button"
+                      color="secondary"
+                      href="/register"
+                      className={classes.link}
+                  >
+                    {"Hello " + localStorage.user}
+                  </Link>
+                  <Button
+                      onClick={handleLogout}
+                      color="secondary"
+                      variant="outlined"
+                      className={classes.link}
+                  >
+                    Logout
+                  </Button>
+                </Toolbar>
+            ) : (
+                <Toolbar className={classes.toolbar}>
+                  <Link className={classes.toolbarLogo} href="/">
+                    <img src={Logo} height="40px" />
+                  </Link>
+                  <Link
+                      variant="button"
+                      color="secondary"
+                      href="/register"
+                      className={classes.link}
+                  >
+                    Register
+                  </Link>
+                  <Button
+                      href="/login"
+                      color="secondary"
+                      variant="outlined"
+                      className={classes.link}
+                  >
+                    Login
+                  </Button>
+                </Toolbar>
+            )}
+          </Container>
+        </AppBar>
+    );
+  }
 }
 
-export default Header;
+export default withStyles(styles)(Header);
--- components/index.js
+++ components/index.js
@@ -1,7 +1,8 @@
 import Footer from "./Footer";
+import SubscribeFooter from "./SubFooter";
 import Header from "./Header";
 import Hero from "./Hero";
 import ReasonsSupport from "./ReasonsSupport";
 import FeaturedApiaries from "./FeaturedApiaries";
 
-export { Footer, Header, Hero, ReasonsSupport, FeaturedApiaries };
+export { SubscribeFooter,  Footer, Header, Hero, ReasonsSupport, FeaturedApiaries };
